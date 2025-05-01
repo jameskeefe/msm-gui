9a4a26f9dd0cde4254e8a99ad0a8087640dc79b8
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-05-01 17:10:40 +0000

    directors controller done as well

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -37,9 +37,35 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/eldest" })
   end
 
+
   def insert_director
+    d = Director.new
+    d.name = params[:the_name]
+    d.dob = params[:the_dob].to_d
+    d.bio = params[:the_bio]
+    d.image = params[:the_image_url]
+    d.save()
 
+    redirect_to("/directors")
+  end
 
+  def delete_director
+    d = Director.where({:id => params[:director_id]}).first()
+    d.destroy()
+
+    redirect_to("/directors")
+  end
+
+  def update_director
+    the_id = params[:director_id]
+    d = Director.where({:id => the_id}).first()
+    d.name = params[:the_name]
+    d.dob = params[:the_dob].to_d
+    d.bio = params[:the_bio]
+    d.image = params[:the_image_url]
+    d.save()
+
+    redirect_to("/directors/#{the_id}")
   end
 
 end
